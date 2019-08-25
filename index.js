5245d4de380b92aaf2b2128df025e97b384f67dd
Valentino Wong <valentino.wong@gmail.com>
2019-08-25 16:13:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let array = [];
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+
+function countdown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num --;
+    }
+}
